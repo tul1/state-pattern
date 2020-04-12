dfa0b4e461211db7ffd9eb2e2caa6cfca71c268a
patricio tula <tula.patricio@gmail.com>
2020-04-12 21:41:18 +0200

    rust book state pattern example

---
+++ src/lib.rs
@@ -0,0 +1,119 @@
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.state.as_ref().unwrap().add_text(&mut self.content, text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(&self)
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+
+    pub fn reject(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.reject())
+        }
+    }
+}
+
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State>;
+
+    fn add_text(&self, post: &mut String, text: &str) {}
+}
+
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {
+            approve_count: 0,
+        })
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        println!("Draft");
+        self
+    }
+
+    fn add_text(&self, content: &mut String, text: &str) {
+        content.push_str(text);
+    }
+}
+
+struct PendingReview {
+    approve_count: u8,
+}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(mut self: Box<Self>) -> Box<dyn State> {
+        self.approve_count += 1;
+        if self.approve_count >= 2 {
+            Box::new(Published {})
+        } else {
+            self
+        }
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        println!("Changing to draft");
+        Box::new(Draft {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use state_pattern::Post;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut post = Post::new();
+
+    post.add_text("Hello Post");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("Hello Post", post.content());
 }
